f3db531a08263e48a6154c0629f363d1dd8eaf10
badireddi satya sai varun <badireddi.ssvarun.civ17@itbhu.ac.in>
2021-06-16 18:48:18 +0530

    to new repo

--- src/main/java/com/Booking/Booking/BookingApplication.java
+++ src/main/java/com/Booking/Booking/BookingApplication.java
@@ -1,8 +1,9 @@
 package com.Booking.Booking;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class BookingApplication {
